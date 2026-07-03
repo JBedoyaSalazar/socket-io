a229530370382b4f8d3ae9ffe79998daad1eb161
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-03 14:32:40 -0500

    feat: enhance socket communication with new greeting features and client disconnection handling

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -2,18 +2,40 @@
 const socket = io();
 
 /* global document */
-const text = document.getElementById("text");
-const MessageToServer = document.getElementById("MessageToServer");
+const text = document.getElementById('text');
+const messageToServer = document.getElementById('MessageToServer');
+const emitToLast = document.getElementById('emit-to-last');
 
-MessageToServer.addEventListener("click", () => {
-    socket.emit("server", "¡Hola, servidor! 😉");
+messageToServer.addEventListener('click', () => {
+    socket.emit('greeting server', '¡Hola, servidor! 😉');
 });
 
-socket.on("welcome", (data) => {
+socket.on('welcome', (data) => {
     console.log(data);
     text.textContent = data;
 });
 
-socket.on("user-connected", (data) => {
+socket.on('user-connected', (data) => {
     console.log(data);
+});
+
+emitToLast.addEventListener('click', () => {
+    socket.emit('greetingToLast', '¡Hola, último cliente! 👋');
+});
+
+socket.on('salute', (data) => {
+    console.log(data);
+});
+
+//on, once, off
+socket.on('on', () => {
+    console.log('Se emite varias veces');
+});
+
+socket.once('once', () => {
+    console.log('Se emite una sola vez');
+});
+
+socket.off('off', () => {
+    console.log('Se desactiva la emisión');
 });
