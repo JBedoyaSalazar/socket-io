fe80c8f8ac38d4d18c4948ca86a0a92acbe290f6
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-05 18:16:41 -0500

    feat: add @socket.io/admin-ui integration for enhanced Socket.IO management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { instrument } from '@socket.io/admin-ui';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,7 +10,20 @@ import { Server } from 'socket.io';
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ['https://admin.socket.io'],
+        credentials: true
+    }
+});
+
+instrument(io, {
+    auth: {
+        type: 'basic',
+        username: 'admin',
+        password: '$2a$12$loos9v3WR89uQPGu88CBO.UPTT0m/AlsIBx9AcV.hsRdNEKAxdoCO'
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -26,4 +40,4 @@ io.on('connection', (socket) => {
 
 httpServer.listen(3000, () => {
     console.log(`Server running on http://localhost:3000`);
-}); 
+});
